3fa47353c0f91ff01a94e3cbf3ce48b5b0b23095
wayne <may36666@gmail.com>
2025-03-25 17:52:40 +0800

    'feat:新增路由組件Home'

--- src/pages/CardDetail.vue
+++ src/pages/CardDetail.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="container mx-auto">
-    <div class="bg-blue-500 w-full h-250 mt-[50px]">
-      <h1 class="text-[50px] text-center"></h1>
-      <div class="border-red-500 border-2 w-full h-[300px] text-2xl">
-
-      </div>
-      <div class="grid ">
-
-      </div>
-    </div>
+  <div
+    class="h-[600px] shadow-xl w-[100%] text-[6px] md:text-base mt-[50px] mb-[200px]"
+  >
+    <img class="h-1/2 w-full" />
+    <ul class="p-3 space-y-5 h-1/2 content-center">
+      <li class="flex flex-row relative space-x-2">
+        <img src="../assets/icons_clock.png" alt="clock" class="absolute top-1" /><span
+          class="pl-[25px]"
+          ></span
+        >
+      </li>
+      <li class="flex flex-row space-x-2">
+        <img src="../assets/icons_pin.png" alt="pin" /><span></span>
+      </li>
+      <li class="grid grid-cols-4 pl-1">
+        <div class="col-span-2 flex flex-row space-x-2">
+          <img src="../assets/icons_phone.png" /><span></span>
+        </div>
+        <div class="col-span-2 flex flex-row space-x-2">
+          <img src="../assets/icons_tag.png" alt="tag" /><span></span>
+        </div>
+      </li>
+    </ul>
   </div>
+</div>
 </template>
 
 <script setup lang="ts" name="CardDetail">
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import {createRouter,createWebHistory} from 'vue-router'
 import CardDetail from '@/pages/CardDetail.vue'
+import Home from "@/pages/Home.vue"
 
 const router =createRouter({
   history:createWebHistory(),
   routes:[
+    {
+      path:'/',
+      name:'首頁',
+      component:Home
+    },
     {
       path:'/CardDetail/:id',
       name:'卡片資訊',
