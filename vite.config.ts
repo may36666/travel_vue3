354d5b0277c15d49cb25d8069984ce307c72f327
may36666 <may36666@gmail.com>
2025-03-27 23:38:55 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/travel_vue3',
+  base:'./travel_vue3',
   plugins: [
     vue(),
     vueDevTools(),
