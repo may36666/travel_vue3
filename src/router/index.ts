c4b474a19ca4df9282634da291b6fbb261e36915
may36666 <may36666@gmail.com>
2025-03-28 01:35:29 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import {createRouter,createWebHistory} from 'vue-router'
+import {createRouter,createWebHashHistory} from 'vue-router'
 import CardDetail from '@/pages/CardDetail.vue'
 import Home from "@/pages/Home.vue"
 
 const router =createRouter({
-  history:createWebHistory(),
+  history:createWebHashHistory(),
   routes:[
     {
       path:'/',
